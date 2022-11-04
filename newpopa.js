dafd3ef144b93efec785aa1579881867f0fff04a
smska2022 <2009sms@rambler.ru>
2022-11-04 16:54:04 +0300

    Работала с Федей

---
+++ newpopa.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const server = express()
+const PORT = 3000
+server.get('/', function (req, res) {
+    res.send('Федя, ты справился!Отлично')
+})
+
+console.log("http://localhost:3000/")
+server.listen(3000)
